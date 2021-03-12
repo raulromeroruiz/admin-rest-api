98d19a16659bc6ab9927159caf7019addd93ec7a
Raúl Romero <raul.romeroruiz@gmail.com>
2021-03-12 00:19:10 -0500

    feat: Despejar administrador

--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -6,18 +6,6 @@
                     <span class="label">Productos</span>
                 </a>
             </div>
-            <div class="col-xs-6 col-sm-4 col-md-3">
-                <a href="../admin/combos" class="btn">
-                    <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-                    <span class="label">Combos</span>
-                </a>
-            </div>
-            <div class="col-xs-6 col-sm-4 col-md-3">
-                <a href="../admin/noticias" class="btn">
-                    <span class="glyphicon glyphicon-globe" aria-hidden="true"></span>
-                    <span class="label">Noticias</span>
-                </a>
-            </div>
         <?php if ($login->tipo != 3) { ?>
             <div class="col-xs-6 col-sm-4 col-md-3">
                 <a href="../admin/usuarios" class="btn">
